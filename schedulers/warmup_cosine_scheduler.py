ef703ab35b15bada121590a396f73c51ee2be724
therealmarvin <nakedtoothbrush@hotmail.com>
2025-03-07 06:37:02 -0500

    fixing warmup cosine scheduler

--- schedulers/warmup_cosine_scheduler.py
+++ schedulers/warmup_cosine_scheduler.py
@@ -1,46 +1,39 @@
 import numpy as np
-
 from torch.optim.lr_scheduler import LRScheduler, CosineAnnealingLR
-
 
 class WarmupCosineScheduler(LRScheduler):
     def __init__(self, optimizer, warmup_steps, total_steps, min_lr=0.0, last_epoch=-1, verbose=False, summary=None):
         self.warmup_steps = warmup_steps
         self.total_steps = total_steps
         self.min_lr = min_lr
-        self.cosine_scheduler = None
         self.summary = summary
+
+        self.cosine_scheduler = CosineAnnealingLR(
+            optimizer,
+            T_max=total_steps - warmup_steps,
+            eta_min=min_lr
+        )
+
         super(WarmupCosineScheduler, self).__init__(optimizer, last_epoch, verbose)
 
     def get_lr(self):
         if self.last_epoch < self.warmup_steps:
-            # Linear warmup phase
             return [
                 base_lr * (self.last_epoch + 1) / float(self.warmup_steps)
                 for base_lr in self.base_lrs
             ]
         else:
-            # Cosine decay phase
-            if self.cosine_scheduler is None:
-                self.cosine_scheduler = CosineAnnealingLR(
-                    self.optimizer,
-                    T_max=self.total_steps - self.warmup_steps,
-                    eta_min=self.min_lr
-                )
-                self.cosine_scheduler.last_epoch = self.last_epoch - self.warmup_steps
-
-            return self.cosine_scheduler.get_lr()
+            return self.cosine_scheduler.get_last_lr()
 
     def step(self, epoch=None):
-        # Advance the scheduler's internal epoch counter
         super(WarmupCosineScheduler, self).step(epoch)
-        # Also step the cosine scheduler after the warmup phase
+
         if self.last_epoch >= self.warmup_steps:
-            self.cosine_scheduler.step(epoch)
+            self.cosine_scheduler.step()
 
         if self.summary is not None:
-            #lr =  self.get_lr()
-            #if type(lr) is list:
-            #    lr = lr[0]
+            lr = self.get_last_lr()
+            if isinstance(lr, list):
+                lr = lr[0]  # Extract scalar if list
 
-            self.summary.add_scalar("learning_rate", np.array(self.get_lr()), self.last_epoch + 1)
+            self.summary.add_scalar("learning_rate", np.array(lr), self.last_epoch + 1)
